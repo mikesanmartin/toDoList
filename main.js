3b39ca1bb0d6b2ba541628ba09468c511959a9ee
Michel Emmanuel San Martin <61049112+mikesanmartin@users.noreply.github.com>
2021-04-05 20:54:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    // Variables
+    var addButton = $("#add");
+    var taskBox = $("#task-box");
+    var taskList = $("#task-list");
+    
+    // Add task
+    addButton.click(function(){
+        var newTask = taskBox.val();
+        
+        if (newTask ===  ''|| newTask === null || newTask === ' '){
+            alert('Please write a new task');
+        }else{
+            console.log("new task added!");
+            taskList.append("<li id='task' class='inprogress'>" + newTask + "</li>");
+            taskBox.val("");
+        };
+    });
+
+    // Complete Task
+    taskList.on('click', 'li', function(){
+        var liElement = $(this);
+
+        if (liElement.hasClass('complete')){
+            liElement.removeClass('complete');
+            liElement.addClass('inprogress');
+        }else{
+            liElement.addClass('complete');
+        }
+        
+    })
+})
